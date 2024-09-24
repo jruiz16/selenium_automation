60109517a096617dfa91def290e13b9ac4f5fe40
Promo <davrpob@gmail.com>
2024-09-24 01:18:18 -0500

    se agregan ambos retos extras iframe y alert

--- src/test/java/com/talleres/Taller_1Test.java
+++ src/test/java/com/talleres/Taller_1Test.java
@@ -1,5 +1,7 @@
 package com.talleres;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,20 +19,21 @@ public class Taller_1Test {
     @Test
     public void reto1() {
 
-        //ARRANGE
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
+        // ARRANGE
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://teststore.automationtesting.co.uk/index.php");
 
-        //ACT
+        // ACT
         WebElement signIn = driver.findElement(By.xpath("//DIV[@class='user-info']//A"));
         signIn.click();
         WebElement noAccount = driver.findElement(By.xpath("//DIV[@class='no-account']//A"));
         noAccount.click();
 
-        //Form
+        // Form
 
         String firstName = "Juan Prueba";
         String lastName = "Ruiz Prueba";
@@ -38,14 +41,22 @@ public class Taller_1Test {
         String password = "ahsfjkahsiuhriyw";
         String birthDate = "05/31/1970";
 
-        WebElement socialTitleInput = driver.findElement(By.xpath("//SPAN[@class='custom-radio']//input[@id='field-id_gender-1']"));
-        WebElement firstNameInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-firstname']"));
-        WebElement lastNameInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-lastname']"));
-        WebElement emailInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-email']"));
-        WebElement passwordInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-password']"));
-        WebElement birthDateInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-birthday']"));
-        WebElement termsInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//input[@name='psgdpr']"));
-        WebElement saveButtonInput = driver.findElement(By.xpath("//form[@class='js-customer-form']//button[@data-link-action='save-customer']"));
+        WebElement socialTitleInput = driver
+                .findElement(By.xpath("//SPAN[@class='custom-radio']//input[@id='field-id_gender-1']"));
+        WebElement firstNameInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-firstname']"));
+        WebElement lastNameInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-lastname']"));
+        WebElement emailInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-email']"));
+        WebElement passwordInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-password']"));
+        WebElement birthDateInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@id='field-birthday']"));
+        WebElement termsInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//input[@name='psgdpr']"));
+        WebElement saveButtonInput = driver
+                .findElement(By.xpath("//form[@class='js-customer-form']//button[@data-link-action='save-customer']"));
 
         socialTitleInput.click();
         firstNameInput.sendKeys(firstName);
@@ -56,28 +67,28 @@ public class Taller_1Test {
         termsInput.click();
         saveButtonInput.click();
 
-        //ASSERT
+        // ASSERT
         WebElement accountName = driver.findElement(By.xpath("//A[@class='account']//span"));
-        assertEquals(firstName+" "+lastName, accountName.getText());
+        assertEquals(firstName + " " + lastName, accountName.getText());
 
     }
 
     @Test
     public void reto2() {
 
-        //ARRANGE
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
+        // ARRANGE
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://www.saucedemo.com/");
 
-        //Login
+        // Login
 
         WebElement inputUser = driver.findElement(By.id("user-name"));
         WebElement inputPassword = driver.findElement(By.id("password"));
         WebElement buttonLogin = driver.findElement(By.id("login-button"));
-
 
         inputUser.sendKeys("standard_user");
         inputPassword.sendKeys("secret_sauce");
@@ -96,7 +107,8 @@ public class Taller_1Test {
                 int randomIndex = random.nextInt(products.size());
                 WebElement product = products.get(randomIndex);
 
-                WebElement priceElement = product.findElement(By.xpath(".//div[@class='pricebar']//div[@class='inventory_item_price']"));
+                WebElement priceElement = product
+                        .findElement(By.xpath(".//div[@class='pricebar']//div[@class='inventory_item_price']"));
                 String priceText = priceElement.getText().replace("$", "");
                 double price = Double.parseDouble(priceText);
 
@@ -104,18 +116,22 @@ public class Taller_1Test {
 
                 System.out.println("Producto seleccionado: $" + price);
 
-                WebElement addToCartButton = product.findElement(By.xpath(".//div[@class='pricebar']//button[contains(@class,'btn_inventory')]"));
+                WebElement addToCartButton = product
+                        .findElement(By.xpath(".//div[@class='pricebar']//button[contains(@class,'btn_inventory')]"));
                 addToCartButton.click();
 
                 products.remove(randomIndex);
             }
 
-            System.out.println("La suma total de los productos seleccionados es: $" + totalSum);
+            System.out.println("La suma total de los productos seleccionados es: $" +
+                    totalSum);
 
-            WebElement cartButton = driver.findElement(By.xpath(".//div[@data-test='header-container']//a[@data-test='shopping-cart-link']"));
+            WebElement cartButton = driver
+                    .findElement(By.xpath(".//div[@data-test='header-container']//a[@data-test='shopping-cart-link']"));
             cartButton.click();
 
-            WebElement checkoutButton = driver.findElement(By.xpath(".//div[@class='cart_footer']//button[@data-test='checkout']"));
+            WebElement checkoutButton = driver
+                    .findElement(By.xpath(".//div[@class='cart_footer']//button[@data-test='checkout']"));
             checkoutButton.click();
 
             WebElement firstNameInput = driver.findElement(By.xpath(".//form//input[@data-test='firstName']"));
@@ -128,9 +144,12 @@ public class Taller_1Test {
             postalCodeInput.sendKeys("05001");
             continueButton.click();
 
-            WebElement subTotalPrice = driver.findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='subtotal-label']"));
-            WebElement taxPrice = driver.findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='tax-label']"));
-            WebElement totalPrice = driver.findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='total-label']"));
+            WebElement subTotalPrice = driver
+                    .findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='subtotal-label']"));
+            WebElement taxPrice = driver
+                    .findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='tax-label']"));
+            WebElement totalPrice = driver
+                    .findElement(By.xpath(".//div[@class='summary_info']//div[@data-test='total-label']"));
 
             String textSubTotalPrice = subTotalPrice.getText();
             String textTaxPrice = taxPrice.getText();
@@ -151,11 +170,49 @@ public class Taller_1Test {
             System.out.println("Total: " + total);
 
             assertEquals(subtotal, totalSum);
-            assertEquals(totalSum+tax, total);
+            assertEquals(totalSum + tax, total);
 
         } else {
             System.out.println("No se encontraron productos suficientes para realizar la prueba.");
         }
     }
 
+    @Test
+    public void retoIframe() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://automationtesting.co.uk/iframes.html");
+
+        WebElement sideBarToggle = driver
+                .findElement(By.xpath("//iframe[@src='index.html']//div[@id='sidebar']/a[@class='toggle']"));
+
+        sideBarToggle.click();
+
+        WebElement listText = driver
+                .findElement(By.xpath("//iframe[@src='index.html']//div[@id='sidebar']//ul/li/a[text()='Accordion']"));
+        listText.click();
+
+        WebElement bigSideBarToggle = driver.findElement(By.xpath("//div[@id='sidebar']//a[@class='toggle']"));
+
+        bigSideBarToggle.click();
+    }
+
+    @Test
+    public void retoAlert() {
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://automationtesting.co.uk/popups.html");
+
+        WebElement buttonTrigger = driver.findElement(By.xpath("//div[@class='row']//button[text()='TriggerAlert']"));
+
+        buttonTrigger.click();
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
 }
